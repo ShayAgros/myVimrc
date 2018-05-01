c3dbff1575613c856819192d4ae7ac6af7a9ff09
Shay Agroskin <shayag@mellanox.com>
2018-05-01 10:28:55 +0300

    Added a script to install vim configuration

---
+++ replace_vimrc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ ! -f `pwd`/.vimrc ||  ! -d `pwd`/vimsources ]]; then
+	echo "script should be ran from the same directory"
+	echo "with .vimrc and vimsources"
+	exit 2
+fi
+
+if [[ -f ~/.vimrc ]]; then
+	echo ".vimrc file found"
+	rm -i ~/.vimrc
+	if [[ -f ~/.vimrc ]]; then
+		exit 1
+	fi
+fi
+
+if [[ -d ~/.vim/plugin ]]; then
+    	echo ".vim/plugin folder found"
+	rm -ir ~/.vim/plugin
+	if [[ -d ~/.vim/plugin ]]; then
+	    exit 1
+	fi
+elif [[ ! -d ~/.vim ]]; then
+    mkdir ~/.vim
+fi
+
+rm -rf ~/vimsources
+
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/vimsources ~/
+ln -s `pwd`/.vim/plugin ~/.vim/plugin
+
+echo ".vimrc file replaced"
